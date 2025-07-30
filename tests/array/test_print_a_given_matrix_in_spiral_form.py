5b47cd72d81f925dca72156f5793fa3a81a65f69
Maria-Papa <1maria.papa@gmail.com>
2025-07-30 16:49:25 +0300

    array: added new input-expected in test

--- tests/array/test_print_a_given_matrix_in_spiral_form.py
+++ tests/array/test_print_a_given_matrix_in_spiral_form.py
@@ -10,6 +10,14 @@ from src.array.print_a_given_matrix_in_spiral_form import print_matrix_in_spiral
             [13, 14, 15, 16]
         ],
         [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+    ),
+    (
+        [
+            [1, 2, 3, 4,  5, 6],
+            [7, 8, 9, 10, 11, 12],
+            [13, 14, 15, 16, 17, 18]
+        ],
+        [1, 2, 3, 4, 5, 6, 12, 18, 17, 16, 15, 14, 13, 7, 8, 9, 10, 11]
     )
 ])
 def test_print_matrix_in_spiral(mat, expected):
